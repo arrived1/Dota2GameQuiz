fd6a14f63cda1d02e0a8c07d5c6d267efcf59d1a
Radoslaw Przybyl <Radoslaw.Przybyl@sabre.com>
2013-11-06 18:04:12 +0000

    class extraction

--- Dota/src/main/java/com/dota/my/DataBaseRecordComparator.java
+++ Dota/src/main/java/com/dota/my/DataBaseRecordComparator.java
@@ -2,33 +2,41 @@ package com.dota.my;
 
 import java.util.Comparator;
 
-public class DataBaseRecordComparator implements Comparator<DataBaseRecord> {
+class DataBaseRecordComparator implements Comparator<DataBaseRecord> {
     @Override
-    public int compare(DataBaseRecord o1, DataBaseRecord o2) {
-        return compareScore(o1, o2);
-    }
-
-    private int compareScore(DataBaseRecord o1, DataBaseRecord o2) {
+    public int compare(DataBaseRecord o1, DataBaseRecord o2)
+    {
         Integer i1 = Integer.parseInt(o1.getScore());
         Integer i2 = Integer.parseInt(o2.getScore());
-        int res = i1.compareTo(i2);
-        if (res == 0)
+        if(i1 == i2)
             return compareTime(o1, o2);
+        else if(i1 < i2)
+            return 1;
         else
-            return res;
+            return -1;
     }
 
     private int compareTime(DataBaseRecord o1, DataBaseRecord o2) {
-        Integer m1 = Integer.parseInt(o1.getTime().substring(0, 1));
-        Integer s1 = Integer.parseInt(o1.getTime().substring(3, 4));
+        String[] time1 = o1.getTime().split(":");
+        String[] time2 = o2.getTime().split(":");
 
-        Integer m2 = Integer.parseInt(o2.getTime().substring(0, 1));
-        Integer s2 = Integer.parseInt(o2.getTime().substring(3, 4));
+        Integer min1 = Integer.parseInt(time1[0]);
+        Integer min2 = Integer.parseInt(time2[0]);
+        Integer sec1 = Integer.parseInt(time1[1]);
+        Integer sec2 = Integer.parseInt(time2[1]);
 
-        Integer resMinute = m1.compareTo(m2);
-        if (resMinute == 0)
-            return resMinute;
+        if(min1 == min2) {
+            if(sec1 == sec2)
+                return 0;
+            else if(sec1 < sec2)
+                return -1;
+            else
+                return 1;
+        }
+        else if(min1 < min2)
+            return -1;
         else
-            return s1.compareTo(s2);
+            return 1;
+
     }
 }
